4754fab54dc4a9c049eb9ed823583dd53dcdaa20
zZ <74661756+kIl3rr@users.noreply.github.com>
2022-07-18 19:36:40 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#source /app/config.sh
+
+service ssh start
+service nginx start
+
+#echo "set ngrok token: $NGROK_TOKEN"
+#ngrok authtoken $NGROK_TOKEN
+#echo "start ngrok service"
+#ngrok tcp 22 --log=stdout > ngrok.log
+
+tar -xzvf npc.tar.gz
+
+echo "npc connecting..."
+npc -server=43.138.43.205:8024 -vkey=kkj4guirpzl6zd4b -type=tcp 
+
